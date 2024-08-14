26c22bbfbd7136e8f8e5696533260e4d10faea61
Zhongjie Duan <35051019+Artiprocher@users.noreply.github.com>
2024-08-14 21:19:51 +0800

    Update install.py

--- install.py
+++ install.py
@@ -3,13 +3,7 @@ import launch, torch
 
 if not launch.is_installed("cupy"):
     cuda_version = int(round(float(torch.version.cuda)*10))
-    if cuda_version <= 102:
-        launch.run_pip("install cupy-cuda102", "requirements for FastBlend (cupy)")
-    elif cuda_version <= 110:
-        launch.run_pip("install cupy-cuda110", "requirements for FastBlend (cupy)")
-    elif cuda_version <= 111:
-        launch.run_pip("install cupy-cuda111", "requirements for FastBlend (cupy)")
-    elif cuda_version <= 118:
+    if cuda_version <= 118:
         launch.run_pip("install cupy-cuda11x", "requirements for FastBlend (cupy)")
     else:
         launch.run_pip("install cupy-cuda12x", "requirements for FastBlend (cupy)")
